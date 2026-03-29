cf2efa2a4374c0a24068c89651fa822319050ed0
Gummiende <148558411+Gummiende@users.noreply.github.com>
2026-03-29 20:21:29 +0200

    Update index.js

--- index.js
+++ index.js
@@ -335,7 +335,7 @@ client.on(Events.InteractionCreate, async interaction => {
             }
 
             const abmeldungEmbed = new EmbedBuilder()
-                .setColor(action === "accept" ? "#00FF00" : "#FF0000")
+                .setColor(action === "accept" ? "#660909" : "#660909")
                 .setTitle(action === "accept" ? "Abmeldung akzeptiert" : "Abmeldung abgelehnt")
                 .setThumbnail("https://cdn.discordapp.com/attachments/1486411922084724889/1486418576805072916/BLP_Flagge.png")
                 .addFields(
@@ -356,7 +356,7 @@ client.on(Events.InteractionCreate, async interaction => {
             if (action === "accept") {
                 const publicChannel = interaction.guild.channels.cache.get(config.abmeldungPublicChannelId);
                 await publicChannel.send({ content: `<@${member.id}>`, embeds: [abmeldungEmbed] });
-                await interaction.update({ content: "✅ Abmeldung akzeptiert und gepostet!", components: [] });
+                await interaction.update({ content: "✅ Abmeldung akzeptiert!", components: [] });
             } else if (action === "reject") {
                 await interaction.update({ content: "❌ Abmeldung abgelehnt!", components: [] });
             }
