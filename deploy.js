9a9016852ad58b089a54c07a1f4921e547d1ee34
Gummiende <148558411+Gummiende@users.noreply.github.com>
2026-03-29 16:38:25 +0200

    Update deploy.js

--- deploy.js
+++ deploy.js
@@ -17,6 +17,29 @@ const commands = [
             option.setName("grund")
                 .setDescription("Grund")
                 .setRequired(true)
+        ),
+
+    new SlashCommandBuilder()
+        .setName("clear")
+        .setDescription("Lösche Nachrichten")
+        .addIntegerOption(option =>
+            option.setName("anzahl")
+                .setDescription("Anzahl der Nachrichten")
+                .setRequired(true)
+        ),
+
+    new SlashCommandBuilder()
+        .setName("kammer")
+        .setDescription("Trage ein Item in die Kammer ein")
+        .addStringOption(option =>
+            option.setName("von")
+                .setDescription("Von")
+                .setRequired(true)
+        )
+        .addStringOption(option =>
+            option.setName("item")
+                .setDescription("Item")
+                .setRequired(true)
         )
 ].map(cmd => cmd.toJSON());
 
@@ -25,10 +48,9 @@ const rest = new REST({ version: "10" }).setToken(process.env.TOKEN);
 (async () => {
     try {
         await rest.put(
-            Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID), // Guild Commands
+            Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID),
             { body: commands }
         );
-
         console.log("✅ Commands registriert!");
     } catch (err) {
         console.error(err);
