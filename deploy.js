6193539e68b7b4b3e84124be6d007609f919ab11
Gummiende <148558411+Gummiende@users.noreply.github.com>
2026-03-30 16:12:10 +0200

    Update deploy.js

--- deploy.js
+++ deploy.js
@@ -17,19 +17,13 @@ const commands = [
             option.setName("grund")
                 .setDescription("Grund")
                 .setRequired(true)
-        ),
-
-    new SlashCommandBuilder()
-        .setName("SetRang")
-        .setDescription("Postet das Rang-Anfrage Panel (nur Führungsebene)")
+        )
 ].map(cmd => cmd.toJSON());
 
 const rest = new REST({ version: "10" }).setToken(process.env.TOKEN);
 
 (async () => {
     try {
-        console.log("CLIENT_ID:", process.env.CLIENT_ID);
-        console.log("GUILD_ID:", process.env.GUILD_ID);
         await rest.put(
             Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID), // Guild Commands
             { body: commands }
