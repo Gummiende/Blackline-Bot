0db1fcf0d2ea94e1ef4ebbc705b1d1c04294b916
Gummiende <148558411+Gummiende@users.noreply.github.com>
2026-03-30 16:03:09 +0200

    Update deploy.js

--- deploy.js
+++ deploy.js
@@ -28,6 +28,8 @@ const rest = new REST({ version: "10" }).setToken(process.env.TOKEN);
 
 (async () => {
     try {
+        console.log("CLIENT_ID:", process.env.CLIENT_ID);
+        console.log("GUILD_ID:", process.env.GUILD_ID);
         await rest.put(
             Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID), // Guild Commands
             { body: commands }
